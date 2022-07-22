4dd066caa2643f4ce2541119339bdf162366b83c
alicee88 <59049601+alicee88@users.noreply.github.com>
2022-07-22 10:51:17 +0100

    Add gamepieces and fill board with random pieces.

--- Match3_Lim/Assets/Scripts/Board.cs
+++ Match3_Lim/Assets/Scripts/Board.cs
@@ -10,15 +10,19 @@ public class Board : MonoBehaviour
     public int borderSize;
 
     public GameObject tilePrefab;
+    public GameObject[] gamePiecePrefabs;
 
     Tile[,] m_allTiles;
+    GamePiece[,] m_allGamePieces;
 
     // Start is called before the first frame update
     void Start()
     {
         m_allTiles = new Tile[width, height];
+        m_allGamePieces = new GamePiece[width, height];
         SetupTiles();
         SetupCamera();
+        FillRandom();
     }
 
     void SetupTiles()
@@ -31,6 +35,7 @@ public class Board : MonoBehaviour
                 tile.name = "Tile (" + i + "," + j + ")";
 
                 m_allTiles[i, j] = tile.GetComponent<Tile>();
+                m_allTiles[i, j].Init(i, j, this);
 
                 // So that if we move the board the tiles will move with it? And to keep the hierarchy tidy
                 tile.transform.parent = transform;
@@ -43,11 +48,46 @@ public class Board : MonoBehaviour
         Camera.main.transform.position = new Vector3((float)((width - 1) / 2f), (float)((height - 1) / 2f), -10f);
 
         float ortho_horizontal = ((float)width / 2 + (float)borderSize) / Camera.main.aspect;
-        Debug.Log("MAIN ASPECT " + Camera.main.aspect);
         float ortho_vertical = ((float)height / 2) + (float)borderSize;
 
         Camera.main.orthographicSize = ortho_horizontal > ortho_vertical ? ortho_horizontal : ortho_vertical;
 
     }
+
+    GameObject GetRandomGamePiece()
+    {
+        int randomIndex = Random.Range(0, gamePiecePrefabs.Length);
+        if (gamePiecePrefabs[randomIndex] == null)
+        {
+            Debug.LogWarning("Oops, you haven't allocated a valid gamepiece prefab to " + randomIndex);
+        }
+        return gamePiecePrefabs[randomIndex];
+    }
+
+    void PlaceGamePiece(GamePiece gamePiece, int x, int y)
+    {
+        if (gamePiece == null)
+        {
+            Debug.LogWarning("BOARD: Invalid game piece!");
+        }
+        gamePiece.transform.position = new Vector3(x, y, 0);
+        gamePiece.transform.rotation = Quaternion.identity;
+        gamePiece.SetCoord(x, y);
+    }
+
+    void FillRandom()
+    {
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < height; j++)
+            {
+                GameObject gamePieceObject = Instantiate(GetRandomGamePiece(), new Vector3(i, j, 0), Quaternion.identity) as GameObject;
+                m_allGamePieces[i, j] = gamePieceObject.GetComponent<GamePiece>();
+                PlaceGamePiece(m_allGamePieces[i, j], i, j);
+            }
+        }
+    }
+
+
 
 }
